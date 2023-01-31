dbca40452458e3f21d93f993774d563501977399
Ed <edgardo.scrimaglia@gmail.com>
2023-01-31 13:43:39 -0300

    config files refactoring

--- plugins/modules/vars.py
+++
@@ -1,7 +0,0 @@
-connection_string = 'BlobEndpoint=https://octionstorage.blob.core.windows.net/;QueueEndpoint=https://octionstorage.queue.core.windows.net/;FileEndpoint=' \
-                      'https://octionstorage.file.core.windows.net/;TableEndpoint=https://octionstorage.table.core.windows.net/' \
-                      ';SharedAccessSignature=sv=2021-06-08&ss=bf&srt=sco&sp=rwdlaciytfx&se=2024-01-01T09:33:09Z&st=2023-01-16T01:33:09Z&spr=' \
-                      'https&sig=RyMyw%2FYnmcgGp7EpucBE6wvjZttCKb7YzeRXAWXPaYA%3D'
-account_name = 'octionstorage'
-account_url = 'https://octionstorage.file.core.windows.net/?sv=2021-06-08&ss=bf&srt=sco&sp=rwdlaciytfx&se=2024-01-01T09:33:09Z&st=2023-01-16T01:33:09Z&spr=https&sig=RyMyw%2FYnmcgGp7EpucBE6wvjZttCKb7YzeRXAWXPaYA%3D'
-access_key = '?sv=2021-06-08&ss=bf&srt=sco&sp=rwdlaciytfx&se=2024-01-01T09:33:09Z&st=2023-01-16T01:33:09Z&spr=https&sig=RyMyw%2FYnmcgGp7EpucBE6wvjZttCKb7YzeRXAWXPaYA%3D'
