6c48215454dbe148626d622481915f75f4144148
Ed <edgardo.scrimaglia@gmail.com>
2023-02-06 00:52:43 -0300

    include test util

--- plugins/modules/test_include_util.py
+++ plugins/modules/test_include_util.py
@@ -63,7 +63,7 @@ import azure.core.exceptions as aze
 from ansible.module_utils.basic import AnsibleModule
 import re
 from azure.storage.fileshare import ShareServiceClient
-from ansible_collections.escrimaglia.o4n_azure_storagefile.plugins.module_utils.util_list_shares import list_shares_in_service
+from ansible_collections.escrimaglia.o4n_azure_storagefile_test.plugins.module_utils.util_list_shares import list_shares_in_service
 
 
 
@@ -94,24 +94,6 @@ def list_files_in_share(_account_name, _connection_string, _share, _dir):
       status = False
 
   return status, msg_ret, output
-
-
-# def list_shares_in_service(_account_name, _connection_string):
-#     output = []
-#     try:
-#         # Instantiate the ShareServiceClient from a connection string
-#         file_service = ShareServiceClient.from_connection_string(_connection_string)
-#         # List the shares in the file service
-#         my_shares = list(file_service.list_shares())
-#         output = [share['name'] for share in my_shares if share]
-#         status = True
-#         msg_ret = f"List of Shares created in account <{_account_name}>"
-#     except Exception as error:
-#         status = False
-#         msg_ret = f"List of Shares not created in account <{_account_name}>. Error: <{error}>"
-
-#     return status, msg_ret, output
-
 
 def main():
   module = AnsibleModule(
